b8197dbb9cf66133e16906f0ff07d0ed56a0e8f2
Wellison Souza <wellison28@gmail.com>
2020-01-20 17:06:21 -0300

    devise setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200120195052) do
+ActiveRecord::Schema.define(version: 20200120200420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,10 +25,17 @@ ActiveRecord::Schema.define(version: 20200120195052) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",       limit: 30, null: false
-    t.string   "last_name",  limit: 50, null: false
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
+    t.string   "name",                   limit: 30,              null: false
+    t.string   "last_name",              limit: 50,              null: false
+    t.datetime "created_at",                                     null: false
+    t.datetime "updated_at",                                     null: false
+    t.string   "email",                             default: "", null: false
+    t.string   "encrypted_password",                default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
---
+++ spec/controllers/reservation_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe ReservationController, type: :controller do
+
+end
