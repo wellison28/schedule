09013a4c9c5df4026df4b252e354186b629280e3
Wellison Souza <wellison28@gmail.com>
2020-01-20 21:52:17 -0300

    auto correction rubocop offenses

--- app/controllers/reservation_controller.rb
+++ app/controllers/reservation_controller.rb
@@ -1,4 +1,5 @@
+# frozen_string_literal: true
+
 class ReservationController < ApplicationController
-  def index
-  end
+  def index; end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,32 +12,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200120200420) do
-
+ActiveRecord::Schema.define(version: 20_200_120_200_420) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "reservations", force: :cascade do |t|
-    t.text     "description"
-    t.datetime "datetime"
-    t.integer  "users_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["users_id"], name: "index_reservations_on_users_id", using: :btree
+  create_table 'reservations', force: :cascade do |t|
+    t.text     'description'
+    t.datetime 'datetime'
+    t.integer  'users_id'
+    t.datetime 'created_at',  null: false
+    t.datetime 'updated_at',  null: false
+    t.index ['users_id'], name: 'index_reservations_on_users_id', using: :btree
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name",                   limit: 30,              null: false
-    t.string   "last_name",              limit: 50,              null: false
-    t.datetime "created_at",                                     null: false
-    t.datetime "updated_at",                                     null: false
-    t.string   "email",                             default: "", null: false
-    t.string   "encrypted_password",                default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  create_table 'users', force: :cascade do |t|
+    t.string   'name',                   limit: 30,              null: false
+    t.string   'last_name',              limit: 50,              null: false
+    t.datetime 'created_at',                                     null: false
+    t.datetime 'updated_at',                                     null: false
+    t.string   'email',                             default: '', null: false
+    t.string   'encrypted_password',                default: '', null: false
+    t.string   'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.index ['email'], name: 'index_users_on_email', unique: true, using: :btree
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true, using: :btree
   end
-
 end
